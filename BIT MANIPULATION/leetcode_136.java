781d358a02032a7c1e3f9e95596f0c270514869a
rishabhhh2005 <your-email@example.com>
2026-02-20 18:38:15 +0530

    Single Number

--- BIT MANIPULATION/leetcode_136.java
+++ BIT MANIPULATION/leetcode_136.java
@@ -1,22 +1,34 @@
-// https://leetcode.com/problems/single-number/description/
-// 136. Single Number
+// // https://leetcode.com/problems/single-number/description/
+// // 136. Single Number
+
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//     Set<Integer> myset = new HashSet<>();
+//     for(int  i=0;i<nums.length;i++){
+//         if(myset.contains(nums[i])){
+//             myset.remove(nums[i]);
+//         }
+//         else{
+//             myset.add(nums[i]);
+//         }
+//     }
+
+//     for( int i : myset){
+//         return i;
+
+//     }
+//     return 0;
+        
+//     }
+// }
+
+// new approach using bit manipulation
 class Solution {
     public int singleNumber(int[] nums) {
-    Set<Integer> myset = new HashSet<>();
-    for(int  i=0;i<nums.length;i++){
-        if(myset.contains(nums[i])){
-            myset.remove(nums[i]);
-        }
-        else{
-            myset.add(nums[i]);
-        }
-    }
-
-    for( int i : myset){
-        return i;
-
-    }
-    return 0;
-        
+        int xor =0;
+        for(int x : nums) xor ^=x;
+        return xor;
     }
 }
+
