d6952d635ee5ba18b28cb08d16577882d04697ac
rishabhhh2005 <your-email@example.com>
2026-02-21 10:20:48 +0530

    Jump Game

--- GREEDY/leetcode_55.java
+++ GREEDY/leetcode_55.java
@@ -2,25 +2,16 @@
 //  Jump Game
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1) return true;
-        if(nums[0] == 0) return false;
-        int  n = nums.length;
-        int targetIndex = n-1; 
-        int p =n-2;
-        
-        while(p>=0){
-            
-            if(p ==0 && p + nums[p] >= targetIndex) return true;
-           
-            if(p + nums[p] >= targetIndex){
-                targetIndex = p;
-                p--;
-              
-                continue;
+        int n  = nums.length;
+        int goal = n-1;
+
+        for(int index = n-2;index>=0;index--){
+            if(index + nums[index] >= goal){
+                goal = index;
             }
-            p--;
         }
-        return false;
+
+        return goal == 0;
         
     }
 }
