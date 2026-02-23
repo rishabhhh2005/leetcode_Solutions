65688f7f1a0f4d559f19aa28ec4f3493814e81cd
rishabhhh2005 <rishabhpuri205@gmail.com>
2026-02-24 00:24:50 +0530

    Invert Binary Tree

---
+++ TREES/leetcode_226.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/invert-binary-tree/description/
+// Invert Binary Tree
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+        
+    }
+}
