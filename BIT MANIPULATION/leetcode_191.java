1b009afa7db278a98ca88e8d0774a393d41b5a4c
rishabhhh2005 <rishabhpuri205@gmail.com>
2026-02-28 12:45:28 +0530

    Number of 1 Bits

---
+++ BIT MANIPULATION/leetcode_191.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/number-of-1-bits/
+// Number of 1 Bits
+class Solution {
+    public int hammingWeight(int n) {
+        int res=0;
+        for(int i=0;i<32;i++){
+            if((1<<i & n ) != 0) res++;
+        }
+        return res;
+        
+    }
+}
