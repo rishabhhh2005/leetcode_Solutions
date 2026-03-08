049ba23341ad01070e7e2c5040c8b0c66e5cdb07
rishabhhh2005 <rishabhpuri205@gmail.com>
2026-03-08 16:17:21 +0530

    Best Time to Buy and Sell Stock IV

---
+++ DP/leetcode_188.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+// Best Time to Buy and Sell Stock IV
+class Solution { // 3D DP QUESTION
+    Integer[][][] dp;
+    public int maxProfit(int k , int[] prices) {
+        // we will try out aall possible ways
+        int n = prices.length;
+        dp = new Integer[n][2][k+1];
+        return helper(0,0,k,prices);
+    }
+    public int helper(int index, int buyed, int limit , int[] prices){
+        if(index >= prices.length || limit ==0) return 0;
+        if(dp[index][buyed][limit] != null) return dp[index][buyed][limit];
+        int p1 =0;   int p2 =0;
+
+        if(buyed == 0){ // if not buyed
+            int case1 = -prices[index] + helper(index+1 , 1, limit,prices); // buy
+            int case2 = 0 + helper(index+1 , 0, limit,prices); // leave
+            p1 = Math.max(case1 , case2);
+        }
+        else{ // if buyed
+            int case1 = prices[index] + helper(index+1 , 0, limit-1,prices); // decrease a transaction when we sell
+            int case2 = 0 + helper(index+1 , 1, limit,prices); // leave
+            p2 = Math.max(case1 , case2);
+            
+        }
+        return dp[index][buyed][limit]=Math.max(p1 , p2); // get the max case
+    }
+}
