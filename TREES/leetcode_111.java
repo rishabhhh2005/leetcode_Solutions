6e90fcb951667810c8c71083172ae1da938064f5
rishabhhh2005 <rishabhpuri205@gmail.com>
2026-03-19 20:54:18 +0530

    Minimum Depth of Binary Tree

---
+++ TREES/leetcode_111.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+// Minimum Depth of Binary Tree
+class Solution {
+    
+    public int minDepth(TreeNode root) {
+        if(root ==null) return 0;
+        Queue<TreeNode > q  = new LinkedList<>();
+        q.offer(root);
+        int depth =0;
+        while(!q.isEmpty()){
+            depth++;
+
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(curr.left == null && curr.right == null) return depth;
+                if(curr.left != null ) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
+            }
+
+        }
+        return depth;
+        
+    }
+}
