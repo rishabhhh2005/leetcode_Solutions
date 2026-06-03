257b6f474dcec5de381c2e005bd3dd4bbe9dd0ff
Rishabh Puri <rishabhpuri205@gmail.com>
2026-06-03 13:03:58 +0530

    Maximum Sum of Distinct Subarrays With Length K

--- SLIDING WINDOW/leetcode_2461.java
+++ SLIDING WINDOW/leetcode_2461.java
@@ -3,24 +3,21 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
         long max = Integer.MIN_VALUE;
-        int maxstart=0; 
         if(k==0) return 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         int n = nums.length;  int left =0;  int right=0;
-
         long currsum =0;
         while(right<n){
             int elem = nums[right];
-            map.put(elem, map.getOrDefault(elem,0)+1);
-            currsum += elem;
-
-            while(map.get(elem) >1 || right-left+1 >k){
+            
+            while( set.contains(elem) || right-left+1 >k){
                 int leftelem = nums[left];
-                map.put(leftelem, map.get(leftelem)-1);
+                set.remove(leftelem);
                 currsum -= leftelem;
-                if(map.get(leftelem) == 0) map.remove(leftelem);
                 left++;
             }
+            set.add(elem);
+            currsum += elem;
             if(right- left +1 == k) max = Math.max(max, currsum);
             right++;   
 
